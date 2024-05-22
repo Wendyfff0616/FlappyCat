62eb7c8759decfb10678d6ce213719dbab8d32c4
Wenyu Fan <wfan0971@uni.sydney.edu.au>
2024-05-22 13:51:51 +1000

    flappycatv2.0

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from pygame import mixer
-from pygame.locals import *
 import random
 
 #initialize pygame
@@ -15,6 +14,10 @@ pygame.mixer.music.play(-1) #-1 means keep playing the music
 pygame.mixer.music.set_volume(0.3)
 jump = mixer.Sound("music/jump.wav")
 jump.set_volume(0.3)
+good = mixer.Sound("music/good.wav")
+good.set_volume(0.3)
+bad = mixer.Sound("music/bad.wav")
+bad.set_volume(1)
 
 #set clock and fps
 clock = pygame.time.Clock()
@@ -28,10 +31,12 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Cat")
 
 #define font
-font = pygame.font.SysFont("Bauhaus 93", 60)
+font_big = pygame.font.SysFont("Bauhaus 93", 60)
+font_small = pygame.font.SysFont("Bauhaus 93", 30)
 
 #define colours
 brown = (115, 74, 18)
+white = (255, 255, 255)
 
 #define game variables(global)
 base_scroll = 0
@@ -39,10 +44,14 @@ scroll_speed = 4
 flying = False
 game_over = False
 pipe_gap = 200
-pipe_frequency = 2800 #millise seconds
+pipe_frequency = 2800  # milliseconds
 last_pipe = pygame.time.get_ticks() - pipe_frequency
+last_flower = pygame.time.get_ticks() - pipe_frequency
+last_bad_flower = pygame.time.get_ticks() - pipe_frequency
 score = 0
+number_of_flowers = 0
 pass_pipe = False
+revive_chance = 0
 
 #load icon
 icon = pygame.image.load("images/icon.png")
@@ -50,18 +59,50 @@ pygame.display.set_icon(icon)
 #load images
 bg = pygame.image.load("images/bg.png")
 base_img = pygame.image.load("images/base.jpg")
-button_img = pygame.image.load("images/start.png")
+revive_img = pygame.image.load("images/revive.png")
+restart_img = pygame.image.load("images/restart.png")
+flower_img = pygame.image.load("images/flower.png")
+bad_flower_img = pygame.image.load("images/bad_flower.png")
 
-def draw_text(text, font, tex_col, x, y):
-    img = font.render(text, True, tex_col)
+
+def draw_text_white(text, font, text_col, x, y, outline_col=(255, 255, 255)):
+    img = font.render(text, True, outline_col)
+    #draw outline by drawing the text offset by 2 pixel in each direction
+    screen.blit(img, (x - 2, y - 2))
+    screen.blit(img, (x + 2, y - 2))
+    screen.blit(img, (x - 2, y + 2))
+    screen.blit(img, (x + 2, y + 2))
+
+    #draw the main text on top
+    img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
+def draw_text_blue(text, font, text_col, x, y, outline_col=(100, 149, 237)):
+    img = font.render(text, True, outline_col)
+    #draw outline by drawing the text offset by 1 pixel in each direction
+    screen.blit(img, (x - 1, y - 1))
+    screen.blit(img, (x + 1, y - 1))
+    screen.blit(img, (x - 1, y + 1))
+    screen.blit(img, (x + 1, y + 1))
+
+    #draw the main text on top
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 def reset_game():
     pipe_group.empty()
+    flower_group.empty()
+    bad_flower_group.empty()
     flappy.rect.x = 200
     flappy.rect.y = int(screen_height / 2)
     score = 0
     return score
+
+def revive_cat():
+    flappy.rect.x = 200
+    flappy.rect.y = int(screen_height / 2)
+    flappy.vel = 0
+    flappy.clicked = False
+    return False  # Set game_over to False
 
 class Cat(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -80,7 +121,7 @@ class Cat(pygame.sprite.Sprite):
 
     def update(self):
 
-        if flying == True:
+        if flying:
             #gravity
             self.vel += 0.5
             if self.vel > 7:
@@ -88,7 +129,7 @@ class Cat(pygame.sprite.Sprite):
             if self.rect.bottom < 380:
                 self.rect.y += int(self.vel)
 
-        if game_over == False:
+        if not game_over:
             #jump
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 self.clicked = True
@@ -130,6 +171,30 @@ class Pipe(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+class Flower(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = flower_img
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.right < 0:
+            self.kill()
+
+class Bad_flower(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bad_flower_img
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.right < 0:
+            self.kill()
+
 class Button():
     def __init__(self, x, y, image):
         self.image = image
@@ -155,12 +220,15 @@ class Button():
 
 cat_group = pygame.sprite.Group()
 pipe_group = pygame.sprite.Group()
+flower_group = pygame.sprite.Group()
+bad_flower_group = pygame.sprite.Group()
 
 flappy = Cat(200, int(screen_height / 2))  #200
 cat_group.add(flappy)
 
 #create restart button instance
-button = Button(screen_width // 2 - 95, screen_height // 2 - 180, button_img)
+restart_button = Button(screen_width // 2 - 95, screen_height // 2 - 180, restart_img)
+revive_button = Button(screen_width // 2 - 95, screen_height // 2 - 180, revive_img)
 
 run = True
 while run:
@@ -172,9 +240,12 @@ while run:
     cat_group.draw(screen)
     cat_group.update()
     pipe_group.draw(screen) #like list and have index
+    flower_group.draw(screen)
+    bad_flower_group.draw(screen)
 
     #draw the base
     screen.blit(base_img, (base_scroll, 380))
+
 
     # Inside the game loop where you check for passing pipes and update the score
     # Check if the cat has passed through a pair of pipes and update the score
@@ -183,14 +254,36 @@ while run:
                 and cat_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right \
                 and pass_pipe == False:
             pass_pipe = True
-        if pass_pipe == True:
+        if pass_pipe:
             if cat_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
                 score += 1
                 print(score)  # Debugging statement to check if the score is being updated
                 pass_pipe = False  # Reset pass_pipe after updating score
 
+    #check for collision with flowers
+    if pygame.sprite.spritecollideany(flappy, flower_group):
+        score += 10
+        number_of_flowers += 1
+        good.play()
+        pygame.sprite.spritecollide(flappy, flower_group, True)
+
+    # check for collision with bad flowers
+    if pygame.sprite.spritecollideany(flappy, bad_flower_group):
+        score -= 5
+        number_of_flowers -= 1
+        bad.play()
+        pygame.sprite.spritecollide(flappy, bad_flower_group, True)
+
     #draw the score
-    draw_text(str(score), font, brown, screen_width//2 - 10, 50)
+    draw_text_white(str(score), font_big, brown, screen_width//2 - 10, 50)
+
+    #draw the number of good flowers
+    #a bad flower cancels out a good flower
+    screen.blit(flower_img, (600, 55))
+    draw_text_blue(str(number_of_flowers), font_small, white, 650, 60)
+
+    #every three good flowers give you a chance of resurrection
+    revive_chance = number_of_flowers // 3
 
     #look for collision
     if pygame.sprite.groupcollide(cat_group, pipe_group, False, False)\
@@ -202,7 +295,7 @@ while run:
         game_over = True
         flying = False
 
-    if game_over == False and flying == True:
+    if not game_over and flying:
         #generate new pipes
         time_now = pygame.time.get_ticks()
         if time_now - last_pipe > pipe_frequency: #free to create an extra pipe
@@ -213,24 +306,50 @@ while run:
             pipe_group.add(top_pipe)
             last_pipe = time_now
 
+        #generate new flowers
+        if time_now - last_flower > pipe_frequency + 500:
+            random_x = random.randint(0, 370)
+            flower_y = random.randint(100, 200)
+            flower = Flower(screen_width + random_x, flower_y)
+            flower_group.add(flower)
+            last_flower = time_now
+
+        #generate new bad flowers
+        if time_now - last_bad_flower > pipe_frequency + 1000:
+            random_x = random.randint(370, 740)
+            bad_flower_y = random.randint(100, 200)
+            bad_flower = Bad_flower(screen_width + random_x, bad_flower_y)
+            bad_flower_group.add(bad_flower)
+            last_bad_flower = time_now
+
         #draw and scroll the base
         base_scroll -= scroll_speed
-        if abs(base_scroll) > 50:
+        if abs(base_scroll) > 1673:
             base_scroll = 0
 
+
         pipe_group.update()
+        flower_group.update()
+        bad_flower_group.update()
 
-
-    #check for game over and reset
-    if game_over == True:
-        if button.draw() == True:
-            game_over = False
-            score = reset_game()
+    # check for game over and reset
+    if game_over:
+        revive_available = revive_chance > 0
+        # check if user has resurrection chances
+        if revive_available:
+            if revive_button.draw():
+                game_over = revive_cat()  #revive the cat and set game_over to False
+                number_of_flowers -= 3  #use three flowers for one revive chance
+        else:
+            if restart_button.draw():
+                game_over = False
+                score = reset_game()
+                number_of_flowers = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+        if event.type == pygame.MOUSEBUTTONDOWN and not flying and not game_over:
             flying = True
 
     pygame.display.update()
